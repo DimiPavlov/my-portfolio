4a28c3ca5d854fbccbac5714e2c2ac05e31c7015
Dimi <64038279+DimiPavlov@users.noreply.github.com>
2021-02-01 20:29:52 +0200

    Updates

---
+++ precache-manifest.41d5d71e5a590d470c0e44522aea5742.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46012f9fdc20bf229829d030b73e44ea",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "da4d4bd240bb3f25e034",
+    "url": "/my-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "851bf16c30dc692539aa",
+    "url": "/my-portfolio/static/css/main.1543bf36.chunk.css"
+  },
+  {
+    "revision": "da4d4bd240bb3f25e034",
+    "url": "/my-portfolio/static/js/2.e473400c.chunk.js"
+  },
+  {
+    "revision": "ea51f729481666395b0d1b3606c9815a",
+    "url": "/my-portfolio/static/js/2.e473400c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851bf16c30dc692539aa",
+    "url": "/my-portfolio/static/js/main.349f65eb.chunk.js"
+  },
+  {
+    "revision": "11a9f05f57c9e93d27fe",
+    "url": "/my-portfolio/static/js/runtime-main.1eb8a9a3.js"
+  },
+  {
+    "revision": "a4fffabdde3071912522de17c646ecc6",
+    "url": "/my-portfolio/static/media/Dimi-Resume.a4fffabd.pdf"
+  }
+]);
